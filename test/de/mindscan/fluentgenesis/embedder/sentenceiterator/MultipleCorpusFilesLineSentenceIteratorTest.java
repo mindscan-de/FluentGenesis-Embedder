45e4618348fc1a07c65d6bc4f532cdea6bb8079e
mindscan-de <maxim.gansert@gmail.com>
2019-11-10 16:44:40 +0100

    test reset and test hasnext methods

--- test/de/mindscan/fluentgenesis/embedder/sentenceiterator/MultipleCorpusFilesLineSentenceIteratorTest.java
+++ test/de/mindscan/fluentgenesis/embedder/sentenceiterator/MultipleCorpusFilesLineSentenceIteratorTest.java
@@ -1,6 +1,8 @@
 package de.mindscan.fluentgenesis.embedder.sentenceiterator;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
 import org.deeplearning4j.text.sentenceiterator.BaseSentenceIterator;
@@ -32,16 +34,44 @@ public class MultipleCorpusFilesLineSentenceIteratorTest {
     }
 
     @Test
-    public void testCreateCorpusIterator_iterateAllFiles() throws Exception {
+    public void testHasNext_iterateAllFiles_numberOfIteratedFilesIsNotZero() throws Exception {
         // arrange
         BaseSentenceIterator iterator = MultipleCorpusFilesLineSentenceIterator.createCorpusIterator( "D:\\Downloads\\Big-Code-excerpt\\1datapoint_1",
                         "(\\.java.json)" );
 
+        int numberOfFiles = 0;
         while (iterator.hasNext()) {
-            String result = iterator.nextSentence();
-            System.out.println( result );
+            iterator.nextSentence();
+            numberOfFiles++;
         }
 
+        assertThat( numberOfFiles, is( not( equalTo( 0 ) ) ) );
+    }
+
+    @Test
+    public void testReset_iterateAllFilesOnceThenResetThenIterateAllAgain_expectNumbersOfIteratedFilesIsSame() throws Exception {
+        // arrange
+        BaseSentenceIterator iterator = MultipleCorpusFilesLineSentenceIterator.createCorpusIterator( "D:\\Downloads\\Big-Code-excerpt\\1datapoint_1",
+                        "(\\.java.json)" );
+
+        int firstRun = 0;
+
+        while (iterator.hasNext()) {
+            // must not be skipped... (yet)
+            iterator.nextSentence();
+            firstRun++;
+        }
+
+        iterator.reset();
+
+        int secondRunAfterReset = 0;
+        while (iterator.hasNext()) {
+            // must not be skipped... (yet)
+            iterator.nextSentence();
+            secondRunAfterReset++;
+        }
+
+        assertThat( secondRunAfterReset, is( equalTo( firstRun ) ) );
     }
 
 }
